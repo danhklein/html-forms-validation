c88e0773d204c86fa968adc7c54ca90a90aba7e4
Daniel Klein <danhirschklein@gmail.com>
2016-01-27 16:06:19 -0700

    Next commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,35 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
-});
+    $('form').on('submit', function() {
+      // alert('Fuck off');
+      event.preventDefault();
+   });
+
+    // function validateEmail (email) {
+
+    //   var emailArr = email.split("");
+    //   if (emailArr[emailArr.length - 1] === '@' || emailArr[emailArr.length - 1] === '.' || emailArr[] ){return false};
+
+    // }
+
+//     ['preferredName', 'lastName'].forEach(function (inputName) {
+//   var $input = $('input[name="' + inputName + "'']");
+
+//    $input.on('keydown', function() {
+//       var val = $input.val();
+//       if (!val) {
+//         $input.addClass('warning');
+//       } else {
+//         $input.removeClass('warning');
+//       }
+//       console.log(val === true)
+//     });
+//   });
+// });
+
+// $email = %'(input)[type="email"]';
+// $email.on('blur', function() {
+//   var val = $email.val();
+//   toggleClass($email, val);
+// })
